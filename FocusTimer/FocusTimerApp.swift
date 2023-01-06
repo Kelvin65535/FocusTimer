c5284389a5f315d277789c300a24754de9655ed8
YeJiayong <kelvin65535@gmail.com>
2023-01-06 20:42:28 +0800

    initial commit

---
+++ FocusTimer/FocusTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusTimerApp.swift
+//  FocusTimer
+//
+//  Created by 叶嘉永 on 2023/1/6.
+//
+
+import SwiftUI
+
+@main
+struct FocusTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
